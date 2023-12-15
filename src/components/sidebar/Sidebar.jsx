e0f0ffbfd0049ae72c6b070b8810e685ffaeece6
Alex Gichau <gichaumburu@gmail.com>
2023-12-15 08:01:42 +0300

    Sidebar fixed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,30 +1,26 @@
 import React, { useState } from 'react';
+import logo from "../../assets/Geeshau-logo.png"
 import './sidebar.css';
 
-const navItems = ['Home', 'About', 'Branding', 'Animations'];
+const navItems = ["home","info","dashboard","contacts","store","Reviews"];
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div className={`sidebar ${isOpen ? 'open' : ""}`}>
       <div className='sidebar-inner'>
         <div className='sidebar-header'>
-          <button
-            type='button'
-            className='sidebar-burger'
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <span>{isOpen ? 'close' : 'menu'}</span>
+          <button type='button' className='sidebar-burger' onClick={() => setIsOpen(!isOpen)}>
+            <span className="material-symbols-outlined"> {isOpen ? 'close' : 'menu_open'}</span>
           </button>
-
-          <img src="../../assets/Geeshau-logo.png" className='sidebar-logo' alt='logo'/>
+          <img src={logo} className='sidebar-logo' alt='Geeshau Art'/>
         </div>
 
         <nav className='sidebar-menu'>
           {navItems.map((item) => (
-            <button key={item} type='button' className='sidebar-button'>
-              <span>(item)</span>
-              <p>(item)</p>
+            <button key={item} type="button" className="sidebar-button">
+              <span className='material-symbols-outlined'>{item}</span>
+              <p>{item}</p>
             </button>
           ))}
         </nav>
