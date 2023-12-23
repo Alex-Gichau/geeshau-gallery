0e7028d86d52dcecae7c6af06bd23a45ff92128b
Alex Gichau <gichaumburu@gmail.com>
2023-12-23 14:02:18 +0300

    hero section added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import "./hero.css";
-import Swiper from 'swiper';
-import { Pagination } from '@mui/material';
 
 export const Hero = () => {
   return (
     <div className='hero-wrapper'>
-      Hero
+      <ul className="slider">
+        <li className="item">
+          <div className="content">
+            <h2 className="title">Lossless Youths</h2>
+            <p className="description">This is a picture that will morph into the background then switch to other features of the website</p>
+            <button>Read More</button>
+          </div>
+        </li>
+        <li className="item">
+          <div className="content">
+            <h2 className="title">Lossless Youths</h2>
+            <p className="description">This is a picture that will morph into the background then switch to other features of the website</p>
+            <button>Read More</button>
+          </div>
+        </li>
+        <li className="item">
+          <div className="content">
+            <h2 className="title">Lossless Youths</h2>
+            <p className="description">This is a picture that will morph into the background then switch to other features of the website</p>
+            <button>Read More</button>
+          </div>
+        </li>
+        <li className="item">
+          <div className="content">
+            <h2 className="title">Lossless Youths</h2>
+            <p className="description">This is a picture that will morph into the background then switch to other features of the website</p>
+            <button>Read More</button>
+          </div>
+        </li>
+      </ul>
+      <nav className="nav">
+        <ion-icon className="prev"><span className='material-symbols-outlined'>Arrow_Back_iOS</span></ion-icon>
+        <ion-icon className="prev"><span className='material-symbols-outlined'>Arrow_Forward_iOS</span></ion-icon>
+      </nav>
     </div>
   )
 }
