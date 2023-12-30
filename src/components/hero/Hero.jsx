00bebddcf7fd46906cc4369cab9af0a1dc4ba05d
Alex Gichau <gichaumburu@gmail.com>
2023-12-30 23:57:57 +0300

    hero slider css updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import './hero.css';
 import logo from "../../assets/Geeshau-logo.png"
 
+import { Swiper,SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverFlow, Pagination, Navigation } from 'swiper';
+
 export const Hero = () => {
   return (
     <div className='header'>
       <div className="header-container">
         <div className="header-left">
-          <h4>THE BEST IN THE MARKET</h4>
+          
+          {/* <h4>THE BEST IN THE MARKET</h4>
           <h1>Take Your Brand to the Next Level</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, quam ipsa? Facere, voluptatum quisquam. Enim assumenda vel, placeat commodi cupiditate maxime animi repellendus iure asperiores explicabo! Minus nemo iure consequatur?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, quam ipsa? Facere, voluptatum quisquam. Enim assumenda vel, placeat commodi cupiditate maxime animi repellendus iure asperiores explicabo! Minus nemo iure consequatur?</p> */}
+
+          
+
         </div>
       </div>
 
