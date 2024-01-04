0343514ab6d26035c75368ec0fe786dc03ef6c4d
Alex Gichau <gichaumburu@gmail.com>
2024-01-04 21:23:48 +0300

    hero page updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import './App.css';
-// import { Sidebar } from "./components/sidebar/Sidebar";
-// import { Magicnav } from './components/magicNav/Magicnav';
-// import { Footer } from "./components/footer/Footer";
-// import { Hero } from './components/hero/Hero';
-// import { Header } from './components/header/Header';
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { Magicnav } from './components/magicNav/Magicnav';
+import { Footer } from "./components/footer/Footer";
+import { Hero } from './components/hero/Hero';
+import { Header } from './components/header/Header';
 import { Testimonials } from './components/testimonials/Testimonials';
-// import { Navigation } from './components/navigation/Navigation';
+import { Navigation } from './components/navigation/Navigation';
 
 const App = () => {
   return (
     <>
-      {/* <Sidebar/> */}
-      {/* <Header/> */}
-      {/* <Navigation/> */}
-      <Testimonials/>
-      {/* <Magicnav/> */}
-      {/* <Footer/> */}
-      {/* <Hero/> */}
+    <div className='grid-container'>
+      {/* <div className='grid-item-1'><Header/></div> */}
+      <div className='grid-item-1'><Navigation/></div>
+      <div className='grid-item-2'><Sidebar/></div>
+      <div className='grid-item-3'><Hero/></div>
+      {/* <div className='grid-item-4'><Testimonials/></div> */}
+      <div className='grid-item-5'><Footer/></div>
+      </div>
     </>
   );
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,94 +1,33 @@
 import React from 'react';
 import './hero.css';
-import logo from "../../assets/Geeshau-logo.png"
-
-import { Swiper,SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import { EffectCoverFlow, Pagination, Navigation } from 'swiper';
+import logo from "../../assets/Geeshau-logo.png";
+import banner from "../../assets/sample.JPG";
+import productsData from "./productsData.json";
 
 export const Hero = () => {
   return (
-    <div className='header'>
-      <div className="header-container">
-        <div className="header-left">
-          
-          {/* <h4>THE BEST IN THE MARKET</h4>
-          <h1>Take Your Brand to the Next Level</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, quam ipsa? Facere, voluptatum quisquam. Enim assumenda vel, placeat commodi cupiditate maxime animi repellendus iure asperiores explicabo! Minus nemo iure consequatur?</p> */}
+    <div className='hero-container'>
+      <div className="hero-banner">
+        <div className="banner-wrapper">
+        <img src={banner} alt="" />
+        </div>
+      </div>
 
-          <div className="container">
-            <h1 className="heading">Gallery Pictures</h1>
-            <Swiper>
-              <SwiperSlide
-              effect={'coverflow'}
-              grabcursor={true}
-              centeredSlides={true}
-              loop={true}
-              slidesPerView={'auto'}
-              coverflowEffect={{
-                rotate:0,
-                stretch:0,
-                depth:100,
-                modifier:2.5,
-              }} 
-              pagination={{el:'.swiper-pagination', clickable:true}}
-              navigation={{
-                nextEl:'swiper-button-next',
-                prevEl:'swiper-button-prev',
-                clickable:'true',
-              }}
-              module={[EffectCoverFlow, Pagination, Navigation]}
-              className='swiper-container'
-              >
-                <img src={logo} alt="logo"/>
-              </SwiperSlide>
+      <div className="products">
+        <div className="product-container">
+          {productsData.map((productsData) => (
+            <li className="product-item" key={productsData.itemCode}>
+                <img src='../../assets/sample.JPG' alt=''/>
+              <p className="item-name">{productsData.products}</p>
+              <p className="product-description">{productsData.description}</p>
+              <p className="charges">{productsData.sellingPrice}</p>
+              <p className="product-code">{productsData.itemCode}</p>
+              <button>Order Now</button>
+            </li>
+          ))}          
 
-              <SwiperSlide>
-                <img src={logo} alt="logo"/>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <img src={logo} alt="logo"/>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <img src={logo} alt="logo"/>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <img src={logo} alt="logo"/>
-              </SwiperSlide>
-
-              <div className="slider-controller">
-                <div className="swiper-button-prev slider-arrow">
-                  <ion-icon name="arrow-back-outline"></ion-icon>
-                </div>
-                <div className="swiper-button-next slider-arrow">
-                  <ion-icon name="arrow-forward-outline"></ion-icon>
-                </div>
-
-                <div className="swiper-pagination"></div>
-
-              </div>
-
-            </Swiper>
           </div>
-          
-
         </div>
       </div>
-
-      <div className="header-right">
-        <div className="circle"></div>
-        <div className="header-img">
-          {/* <img src={logo} alt="" /> */}
-        </div>
-      </div>
-
-    </div>
   );
 };
