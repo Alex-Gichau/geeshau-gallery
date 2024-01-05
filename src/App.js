5bf09ddd7f162206257754870d07c5afdf9c081f
Alex Gichau <gichaumburu@gmail.com>
2024-01-05 22:38:33 +0300

    ratings sidebar updated

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { Magicnav } from './components/magicNav/Magicnav';
 import { Footer } from "./components/footer/Footer";
 import { Hero } from './components/hero/Hero';
 import { Header } from './components/header/Header';
-import { Testimonials } from './components/testimonials/Testimonials';
+// import { Testimonials } from './components/testimonials/Testimonials';
 import { Navigation } from './components/navigation/Navigation';
+import { Ratings } from "./components/testimonials/Ratings"
 
 const App = () => {
   return (
@@ -16,7 +17,7 @@ const App = () => {
       <div className='grid-item-1'><Navigation/></div>
       <div className='grid-item-2'><Sidebar/></div>
       <div className='grid-item-3'><Hero/></div>
-      {/* <div className='grid-item-4'><Testimonials/></div> */}
+      <div className='grid-item-4'><Ratings/></div>
       <div className='grid-item-5'><Footer/></div>
       </div>
     </>
