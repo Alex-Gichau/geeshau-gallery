3e7d524cd36a4d6ae7ae960ec5ae7ab9cd0d5299
Alex Gichau <gichaumburu@gmail.com>
2024-01-15 22:08:40 +0300

    cards design updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
     <div className='hero-container'>
       <div className="hero-banner">
         <div className="banner-wrapper">
-        <img src={banner} alt="" />
+          <img src={banner} alt="" />
         </div>
       </div>
 
@@ -17,22 +17,30 @@ export const Hero = () => {
         <div className="product-container">
           {productsData.map((productsData) => (
             <li className="product-card" key={productsData.itemCode}>
-                <img src='../../assets/sample.JPG' alt=''/>
-              <p className="item-name">{productsData.products}</p>
-              <ul className="format">
-                <li className="format-type">{productsData.formats-1}</li>
-                <li className="format-type">{productsData.formats-2}</li>
-                <li className="format-type">{productsData.formats-3}</li>
-              </ul>
+              <img src='../../assets/sample.JPG' alt='' />
+              <div className="product-title-container">
+                <p className="product-code">{productsData.itemCode}</p>
+                <p className="item-name">{productsData.products}</p>
+              </div>
               <p className="product-description">{productsData.description}</p>
-              <p className="charges">Kes. {productsData.sellingPrice}</p>
-              <p className="product-code">{productsData.itemCode}</p>
+              <hr/>
+              <div className="card-footer">
+                
+                <p className="charges">Kes. {productsData.sellingPrice}</p>
+
+                <ul className="format">
+                  <li className="format-type">{productsData.formats - 1}</li>
+                  <li className="format-type">{productsData.formats - 2}</li>
+                  <li className="format-type">{productsData.formats - 3}</li>
+                </ul>
+              </div>
+
               <button>Order Now</button>
             </li>
-          ))}          
+          ))}
 
-          </div>
         </div>
       </div>
+    </div>
   );
 };
