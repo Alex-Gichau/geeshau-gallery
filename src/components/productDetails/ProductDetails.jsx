ac4eb122480973ef70fc47a8d736e18f14f47942
Alex Gichau <gichaumburu@gmail.com>
2024-01-17 21:57:16 +0300

    product images aligned

--- src/components/productDetails/ProductDetails.jsx
+++ src/components/productDetails/ProductDetails.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import "./productDetails.css"
-import {Footer} from "../footer/Footer"
+import { Footer } from "../footer/Footer"
 
 export const ProductDetails = () => {
   return (
     <div className="details-wrapper">
-        <h1>Product Details</h1>
-        <div className="product-image-container">
-          <div className="img-main">
-            <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
-          </div>
-          <div className="img-preview">
+      <h1>Product Details</h1>
+      <div className="product-image-container">
+        <div className="img-main-container">
           <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
-          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
-          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
-          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
-          </div>
         </div>
-        <div className="product-details">
-          <h1>Posters</h1>
+        <div className="img-preview-container">
+          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
+          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
+          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
+          <img src="https://mir-s3-cdn-cf.behance.net/projects/404/1acf6e140736935.Y3JvcCwzMTcwLDI0ODAsMTY0LDA.jpg" alt="product" />
         </div>
+      </div>
+      <div className="product-details">
+        <h1>Posters</h1>
+      </div>
     </div>
   )
-} 
+}
 
 // const Footer = () => {
 //   return (
