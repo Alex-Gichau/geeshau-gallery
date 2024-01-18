5f3db45278a422d63e1a9fa5f132353257f77b53
Alex Gichau <gichaumburu@gmail.com>
2024-01-18 21:36:33 +0300

    pages folder created

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { Sidebar } from "./components/sidebar/Sidebar";
 // import { Magicnav } from './components/magicNav/Magicnav';
 // import { Footer } from "./components/footer/Footer";
-import { Hero } from './components/hero/Hero';
-import { ProductDetails } from './components/productDetails/ProductDetails';
+import { Hero } from './pages/hero/Hero';
+import { ProductDetails } from './pages/productDetails/ProductDetails';
 // import { Header } from './components/header/Header';
 // import { Testimonials } from './components/testimonials/Testimonials';
 // import { Navigation } from './components/navigation/Navigation';
@@ -14,14 +14,9 @@ import { ProductDetails } from './components/productDetails/ProductDetails';
 const App = () => {
   return (
     <BrowserRouter>
-        <Routes className='grid-container'>
-          {/* <div className='grid-item-1'><Header/></div> */}
-          {/* <div className='grid-item-1'><Navigation/></div> */}
-          {/* <div className='grid-item-2'><Sidebar/></div> */}
-          <Route className='grid-item-3'path='/' element={<Hero/>}/>
+        <Routes>
+          <Route path='/' element={<Hero/>}/>
           <Route path='/:productItemCode' element={<ProductDetails/>}/>
-          {/* <div className='grid-item-4'><Ratings/></div> */}
-          {/* <div className='grid-item-5'><Footer/></div> */}
         </Routes>
     </BrowserRouter>
   );
