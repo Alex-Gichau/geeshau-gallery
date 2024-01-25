2275a43ab2def465423c417e592f96383d80a9eb
Alex Gichau <gichaumburu@gmail.com>
2024-01-26 00:12:18 +0300

    hero added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./hero.css"
 
-export const hero = () => {
+export const Hero = () => {
   return (
-    <div className='hero-container'>
-        
+    <div className='hero-wrapper'>
+      <div className="hero-title">
+        <h1>The New Age of</h1>
+        <h1>Design</h1>
+        <h1>is with Us</h1>
+        <div className="buttons-container">
+          <input type="button" className='explore-btn'
+            name='Explore Artwork' />
+          <input type="button" className='aboutus-btn' name='About Us' />
+        </div>
+      </div>
+      <div className="hero-img">
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./home.css"
-// import Navigation from "../../components/navigation/Navigation"
-import Hero from "../../components/hero/Hero"
+// import { Navigation } from "../../components/navigation/Navigation"
+import { Hero } from "../../components/hero/Hero"
 
 export const Home = () => {
   return (
     <div>
-      <Navigation/>
-      <Hero/>        
+      {/* <Navigation/> */}
+      <Hero/>
     </div>
-  )
-} 
+  );
+} ;
