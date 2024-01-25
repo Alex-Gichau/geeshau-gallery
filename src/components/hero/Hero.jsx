5bec9a3fbb310b2e1ac423fde62aa0175ae8a48f
Alex Gichau <gichaumburu@gmail.com>
2024-01-26 00:53:43 +0300

    hero designing

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,19 +3,20 @@ import "./hero.css"
 
 export const Hero = () => {
   return (
-    <div className='hero-wrapper'>
-      <div className="hero-title">
-        <h1>The New Age of</h1>
-        <h1>Design</h1>
-        <h1>is with Us</h1>
-        <div className="buttons-container">
-          <input type="button" className='explore-btn'
-            name='Explore Artwork' />
-          <input type="button" className='aboutus-btn' name='About Us' />
+    <header className='main-container'>
+      <div className='hero-wrapper'>
+        <div className="hero-title">
+          <h4>#1 Best Social Media Designs</h4>
+          <h1>The New Age of Design is With Us</h1>
+          <p className="hero-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste harum est laborum, sit aspernatur ad rem possimus saepe officia, veniam non error id perferendis nisi. Perferendis voluptatem molestiae vero.</p>
+          <div className="buttons-container">
+            <input type="button" className='explore-btn'
+              name='Explore Artwork' />
+            <input type="button" className='aboutus-btn' name='About Us' />
+          </div>
         </div>
+        <div className="hero-img"/>
       </div>
-      <div className="hero-img">
-      </div>
-    </div>
+    </header>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./home.css"
-// import { Navigation } from "../../components/navigation/Navigation"
+import { Navigation } from "../../components/navigation/Navigation"
 import { Hero } from "../../components/hero/Hero"
 
 export const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
       {/* <Navigation/> */}
       <Hero/>
     </div>
