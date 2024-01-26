6e22d05745c17ae6ac4068f4c59f72960cd8c509
Alex Gichau <gichaumburu@gmail.com>
2024-01-26 22:29:57 +0300

    hero page drafted

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import "./hero.css"
+import heroImage from "../../assets/hero.jpeg"
 
 export const Hero = () => {
   return (
     <header className='main-container'>
       <div className='hero-wrapper'>
         <div className="hero-title">
-          <h4>#1 Best Social Media Designs</h4>
-          <h1>The New Age of Design is With Us</h1>
+          <h4 className='hero-hashtag'>#1 Best Social Media Designs</h4>
+          <h1 className='hero-text'>The New Age of Design With Geeshau Art</h1>
           <p className="hero-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste harum est laborum, sit aspernatur ad rem possimus saepe officia, veniam non error id perferendis nisi. Perferendis voluptatem molestiae vero.</p>
           <div className="buttons-container">
             <input type="button" className='explore-btn'
-              name='Explore Artwork' />
-            <input type="button" className='aboutus-btn' name='About Us' />
+              value='Explore Artwork' />
+            <input type="button" className='aboutus-btn' value='About Us'/>
           </div>
         </div>
-        <div className="hero-img"></div>
+        <div className="hero-img">
+          <img src={heroImage} alt="Hero" />
         </div>
+      </div>
     </header>
   )
 }
