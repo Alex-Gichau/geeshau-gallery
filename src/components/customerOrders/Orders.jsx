ce850034db9e15db2dcd60d9f233cad9af2c0ad5
Alex Gichau <gichaumburu@gmail.com>
2024-01-28 06:12:16 +0300

    orders slider linked to JSON

--- src/components/customerOrders/Orders.jsx
+++ src/components/customerOrders/Orders.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./orders.css"
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import data from "./ordersData.json"
 
 export const Orders = () => {
+  // const [ordersData, setOrdersData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch('./ordersData.json');
+  //     const data = await response.json();
+  //     setOrdersData(data);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  const ordersData = data.data;
 
   const settings = {
-    className:"center",
+    className: "center",
     dots: true,
     infinite: true,
     speed: 500,
@@ -15,113 +29,24 @@ export const Orders = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    pauseonHover:true,
-    centerPadding:"60px"
+    pauseonHover: true,
+    centerPadding: "60px"
   };
 
   return (
     <div className="order-container">
       <Slider {...settings}>
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
+        {ordersData.map((order, id) => (
+        <div className="client-order" key={id}>
+          <img src={order.image} alt="" className="client-logo" />
           <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
+            <h1 className="client-name">{order.name}</h1>
+            <p className="client-desc">{order.request}</p>
+            <span className="order-date">{order.orderDate}</span>
           </div>
         </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
-        <div className="client-order">
-          <img src="https://www.salesforce.com/content/dam/web/en_us/www/images/resource-center/article-what-Is-customer-service.jpg" alt="" className="client-logo" />
-          <div className="order-details">
-            <h1 className="client-name">Just In Faith</h1>
-            <p className="client-desc">
-              I need 100 business cards and a social media poster for an event taking place this weekend at PCEA Juja Township Church. When can you deliver and how much does it cost?
-            </p>
-            <span className="order-date">2 months ago</span>
-          </div>
-        </div>
-
+        ))}
       </Slider>
     </div>
   )
-}
+};
