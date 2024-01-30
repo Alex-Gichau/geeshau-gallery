a31094fa9972171c1d21fdc9b2cb6029790ed9db
Alex Gichau <gichaumburu@gmail.com>
2024-01-30 16:58:42 +0300

    productsAd updated

--- src/components/productsAd/ProductsAd.jsx
+++ src/components/productsAd/ProductsAd.jsx
@@ -4,7 +4,7 @@ import ProductsAdData from "./productsAd.css"
 
 export const ProductsAd = () => {
 
-    const productsAd = ProductsAdData.dataAsImages;
+    // const productsAd = ProductsAdData.dataAsImages;
 
     return (
         <div className="gallery-container">
@@ -18,13 +18,57 @@ export const ProductsAd = () => {
                 <img src={productsAd.image} alt={productsAd.name} key={id} />
             ))} */}
             <div className="images-container">
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
-                <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
+                <div className="img-container">
+                    <div className="image-box">
+                        <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
+                    </div>
+
+                    <div className="image-desc">
+                        <h2 className="client-name">
+                            Company Name<br/>
+                            <span>Social Media Poster</span>
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="img-container">
+                    <div className="image-box">
+                        <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
+                    </div>
+
+                    <div className="image-desc">
+                        <h2 className="client-name">
+                        Company Name<br/>
+                            <span>Social Media Poster</span>
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="img-container">
+                    <div className="image-box">
+                        <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
+                    </div>
+
+                    <div className="image-desc">
+                        <h2 className="client-name">
+                        Company Name<br/>
+                            <span>Social Media Poster</span>
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="img-container">
+                    <div className="image-box">
+                        <img src="http://localhost:3000/static/media/ai%20image%202.c9f168234ee5d616070a.jpeg" alt="" />
+                    </div>
+
+                    <div className="image-desc">
+                        <h2 className="client-name">
+                        Company Name<br/>
+                            <span>Social Media Poster</span>
+                        </h2>
+                    </div>
+                </div>
             </div>
         </div>
     )
