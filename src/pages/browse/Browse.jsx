0022b8898a5f3697a1065b66711e5206eaf4e2b6
Alex Gichau <gichaumburu@gmail.com>
2024-02-11 00:00:26 +0300

    browse grid updated

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./browse.css"
-import { useEffect, useState } from "react"
 import { Search } from '../../components/search/Search'
+import gallery from "../../pages/browse/productsGallery.json"
 
 
 export const Browse = () => {
 
-    
+    const [model, setModel] = useState(false);
+    const [tempImgSrc, setTempImgSrc] = useState('');
 
-    return (
-        <div className='browse-container'>
-            <div className="header-banner">
-            </div>
+    const getImage = (image_url) => {
+        setTempImgSrc(image_url);
+        setModel(true);
 
-            <Search />
+        return (
+            // <div className={model ? "model open" : "model"}>
+            //     <img src={tempImgSrc}/>
+            // https://www.youtube.com/watch?v=cDwa_JwuC-w&t=255s
+            // </div>
 
-            <div className="products-grid">
-                <h1 className='grid-title'>Browse Our Products</h1>
-                <div className="gallery-wrapper">
-                    <div className="galleryContainer">
-                        {
-                            
-                        }
-                    </div>
-
+            <div className='browse-container'>
+                <div className="header-banner">
                 </div>
-            </div>
 
-        </div>
-    )
+                <Search />
+
+                <div className="products-grid">
+                    <h1 className='grid-title'>Browse Our Products</h1>
+                    <div className="gallery-wrapper">
+                        <div className="galleryContainer">
+                            {
+                                gallery.map((gallery) => <div className='galleryItem'>
+                                    <img src={gallery.image_url} key={gallery.time} onClick={() => getImage(gallery.image_url)} />
+                                </div>)
+                            }
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
 }
