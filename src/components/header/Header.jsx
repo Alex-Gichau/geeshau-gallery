9590f57ddaf7bcedc38b70292a87b96c2ae7c3ab
Alex Gichau <gichaumburu@gmail.com>
2024-02-13 23:31:03 +0300

    header to editted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,56 +1,10 @@
 import React from 'react';
 import './header.css';
-
 import image1 from '../../assets/pen.svg';
 import { Pagination } from 'swiper/modules';
 import { Swiper,SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-const slides = [
-  {
-    name1: 'Designs for Social Media',
-    image1: image1,
-    items1: [
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar branding',
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar',
-    ],
-  },
-  {
-    name2: 'Designs for Print',
-    image2: image1,
-    items2: [
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar branding',
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar',
-    ],
-  },
-  {
-    name3: 'Designs for UI/UX',
-    image3: image1,
-    items3: [
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar branding',
-      'Posters',
-      'Banners',
-      'YouTube',
-      'Webinar',
-    ],
-  },
-];
 
 export const Header = () => {
   return (
