85d6e7d93ec8ffaf20e74cfbf16e472b5a6dd1c8
Alex Gichau <gichaumburu@gmail.com>
2024-02-13 23:05:50 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -54,42 +54,9 @@ const slides = [
 
 export const Header = () => {
   return (
-    <div className='header-wrapper'>
-      <nav>
-        <img src={image1} />
-        <div className='nav-items'>
-          <a href='#'>Portfolio</a>
-          <a href='#'>About</a>
-          <a href='#'>Contact</a>
-
-          <div className='dropdown'>
-            <button>What we design</button>
-            <div className='menu'>
-              <Swiper
-                modules={[Pagination]}
-                navigation
-                loop
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-              >
-                {slides?.map((slide) => (
-                  <SwiperSlide key={slide.name1}>
-                    <img src={slide.image1} />
-                    <div>
-                      <h2>{slide.name1}</h2>
-                      <div className='links'>
-                        {slide.items1?.map((items1) => (
-                          <span>(items1)</span>
-                        ))}
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          </div>
-        </div>
-      </nav>
+    <div>
+      
     </div>
-  );
+    
+  )
 };
