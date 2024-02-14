f8933d3e066ec70e8e6029f92efc9f3456d5a93e
Alex Gichau <gichaumburu@gmail.com>
2024-02-14 23:25:03 +0300

    geeshau bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './header.css';
-import image1 from '../../assets/pen.svg';
-import { Pagination } from 'swiper/modules';
-import { Swiper,SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
+
 
 export const Header = () => {
   return (
