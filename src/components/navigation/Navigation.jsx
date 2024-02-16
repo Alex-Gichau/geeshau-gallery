562cbcf7351eabfdc0f991fbf4e6a1d0d3ba5d89
Alex Gichau <gichaumburu@gmail.com>
2024-02-16 11:15:36 +0300

    navigation css updated

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,98 +4,57 @@ import geeshau from "../../assets/Geeshau-logo.png"
 
 export const Navigation = () => {
   return (
-    <div className='wrapper'>
-      <header className='header'>
-        <div className='menu-wrapper'>
-          <div className='menu-bar'>
-            <a href='#Home' title='Home' className='logo'>
-              <img src={geeshau} alt='logo' /> 
+    <header className='container'>
+      <a href="#" className='nav__logo'>Geeshau</a>
+
+      <div className="nav__menu" id='nav-menu'>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+              <span className='icon'></span>
+              <span className="nav__name">Home</span>
             </a>
-            <nav>
-              <ul className='navigation'>
-              <li>
-                  <a href='#devs' title='Developers'>
-                    Explore 
-                  </a>
-                </li>
-                <li>
-                  <a href='#devs' title='Developers'>
-                    Developers
-                  </a>
-                </li>
-                <li>
-                  <button type='button'>Product</button>
-                  <div className='dropdown_wrapper'>
-                    <div className='dropdown'>
-                      <ul className='list-items-with-description'>
-                        <li>
-                          <div className='icon-wrapper'>
-                            <span className='icon'>
-                              <ion-icon>Home</ion-icon>
-                            </span>
-                          </div>
-                          <div className='item-title'>
-                            <h3>Database</h3>
-                            <p>Fully portable postgres</p>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='icon-wrapper'>
-                            <span className='icon'>
-                              <ion-icon>Home</ion-icon>
-                            </span>
-                          </div>
-                          <div className='item-title'>
-                            <h3>Database</h3>
-                            <p>Fully portable postgres</p>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='icon-wrapper'>
-                            <span className='icon'>
-                              <ion-icon>Home</ion-icon>
-                            </span>
-                          </div>
-                          <div className='item-title'>
-                            <h3>Database</h3>
-                            <p>Fully portable postgres</p>
-                          </div>
-                        </li>
-                        <li>
-                          <div className='icon-wrapper'>
-                            <span className='icon'>
-                              <ion-icon>Home</ion-icon>
-                            </span>
-                          </div>
-                          <div className='item-title'>
-                            <h3>Database</h3>
-                            <p>Fully portable postgres</p>
-                          </div>
-                        </li>
-                        
-                      </ul>
-                    </div>
-                  </div>
-                </li>
-                
-              </ul>
-            </nav>
-          </div>
-          <div className='action-buttons'>
-            <a href='#log-in' title='Log in' className='secondary'>
-              Sign In
+          </li>
+
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+              <span className='icon'></span>
+              <span className="nav__name">Home</span>
             </a>
-            <a href='#sign-up' title='Log in' className='primary'>
-              Start your project
+          </li>
+
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+              <span className='icon'></span>
+              <span className="nav__name">Home</span>
             </a>
-          </div>
-          <button
-            aria-label='Open menu'
-            className='burger-menu'
-            type='button'
-          ></button>
-        </div>
-      </header>
-    </div>
+          </li>
+
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+              <span className='icon'></span>
+              <span className="nav__name">Home</span>
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+              <span className='icon'></span>
+              <span className="nav__name">Home</span>
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a href="#" className='nav__link'>
+            <span className='icon'></span>
+              <span className="nav__name">Home</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <img src={geeshau} alt="" className='nav__img'/>
+      
+    </header>
   );
 };
